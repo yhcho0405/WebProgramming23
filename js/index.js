689ebf7c93166115c8ea1cfb149f8a73f2aa5860
Huni <gnsl505@naver.com>
2023-11-29 16:43:46 +0900

    그냥 화딱지 나서 관리자 로그인 그딴거버림

--- js/index.js
+++ js/index.js
@@ -1,15 +1,7 @@
-const loginForm = document.getElementById("login-form");
-
 function login() {
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
 
     // 간단한 로그인 체크
-    if (username === 'admin' && password === 'admin') {
-        alert('관리자로 로그인 되었습니다');
-        window.location.href = '../index.html';
-    } else{
         alert('로그인 되었습니다');
         window.location.href = '../index.html';
-    }
+
 }
